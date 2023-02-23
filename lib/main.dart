0123d611d3981d30f042c909b3d8bdb274adeec2
kristiyana milcheva <kristiyana.milcheva@gmail.com>
2023-02-23 18:30:50 +0100

    added a provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import './screens/product_info_screen.dart';
 import './screens/products_overview_screen.dart';
+import './providers/products_provider.dart';
 
 void main(List<String> args) {
   runApp(const MyApp());
@@ -12,17 +14,18 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'MyShop',
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-        accentColor: Colors.deepOrange,
-        fontFamily: 'Lato',
+    return ChangeNotifierProvider(
+      create: (ctx) => Products(),
+      child: MaterialApp(
+        title: 'MyShop',
+        theme: ThemeData(
+          primarySwatch: Colors.purple,
+          accentColor: Colors.deepOrange,
+          fontFamily: 'Lato',
+        ),
+        home: ProductsOverviewScreen(),
+        routes: {ProductInfoScreen.routeName: (ctx) => ProductInfoScreen()},
       ),
-      home: ProductsOverviewScreen(),
-      routes: {
-        ProductInfoScreen.routeName: (ctx) => ProductInfoScreen()
-      },
     );
   }
 }
