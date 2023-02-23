14c7c779a700948b45d479bc5beccfb509c1ba17
kristiyana milcheva <kristiyana.milcheva@gmail.com>
2023-02-23 16:53:38 +0100

    initial commit, product model ready

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MyShop',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('My Shop'),
+      ),
+      body: const Center(
+        child: Text('yo'),
+      ),
+    );
+  }
+}
---
+++ lib/screens/products_overview_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ProductsOverviewScreen extends StatelessWidget {
+  const ProductsOverviewScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
