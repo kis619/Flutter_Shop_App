377b34f9905d310c9f9cefb20371167dc4b1eba1
kristiyana milcheva <kristiyana.milcheva@gmail.com>
2023-02-23 17:20:04 +0100

    layout for the products_overview_screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './screens/products_overview_screen.dart';
+
 void main(List<String> args) {
   runApp(const MyApp());
 }
@@ -14,23 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('My Shop'),
-      ),
-      body: const Center(
-        child: Text('yo'),
-      ),
+      home: ProductsOverviewScreen(),
     );
   }
 }
