1f88987d8dc300f6f0704def9c2d45ca6eebbd2a
kristiyana milcheva <kristiyana.milcheva@gmail.com>
2023-02-23 17:33:25 +0100

    basic theming

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -8,20 +8,33 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridTile(
-      footer: GridTileBar(
-        backgroundColor: Colors.black54,
-        leading: IconButton(onPressed: () {}, icon: const Icon(Icons.favorite)),
-        title: Text(
-          title,
-          textAlign: TextAlign.center,
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        footer: GridTileBar(
+          backgroundColor: Colors.black87,
+          leading: ClipRRect(
+            borderRadius: BorderRadius.circular(50),
+            child: IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.favorite),
+              color: Theme.of(context).accentColor,
+            ),
+          ),
+          title: Text(
+            title,
+            textAlign: TextAlign.center,
+          ),
+          trailing: IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.shopping_cart),
+            color: Theme.of(context).accentColor,
+          ),
         ),
-        trailing:
-            IconButton(onPressed: () {}, icon: const Icon(Icons.shopping_cart)),
-      ),
-      child: Image.network(
-        imageUrl,
-        fit: BoxFit.cover,
+        child: Image.network(
+          imageUrl,
+          fit: BoxFit.cover,
+        ),
       ),
     );
   }
