af20adb9482b81c519b58f13b41fa5cb9a06735d
kristiyana milcheva <kristiyana.milcheva@gmail.com>
2023-02-27 10:17:40 +0100

    added data base and making requests

--- lib/providers/products_provider.dart
+++ lib/providers/products_provider.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
-import 'product_provider.dart';
+import 'package:http/http.dart' as http;
+
+import './product_provider.dart';
 
 class Products with ChangeNotifier {
   // ignore: prefer_final_fields
@@ -51,14 +55,28 @@ class Products with ChangeNotifier {
   }
 
   void addProduct(Product product) {
-    final newProduct = Product(
-        id: DateTime.now().toString(),
-        title: product.title,
-        description: product.description,
-        price: product.price,
-        imageUrl: product.imageUrl);
-    _items.add(newProduct);
-    notifyListeners();
+    final url = Uri.parse(
+        "https://learning-flutter-72888-default-rtdb.europe-west1.firebasedatabase.app/products.json");
+    http
+        .post(url,
+            body: json.encode({
+              'title': product.title,
+              'description': product.description,
+              'imageUrl': product.imageUrl,
+              'price': product.price,
+              'isFavourite': product.isFavourite,
+            }))
+        .then((response) {
+      print(json.decode(response.body));
+      final newProduct = Product(
+          id: json.decode(response.body)['name'],
+          title: product.title,
+          description: product.description,
+          price: product.price,
+          imageUrl: product.imageUrl);
+      _items.add(newProduct);
+      notifyListeners();
+    });
   }
 
   void updateProduct(String productId, Product newProduct) {
